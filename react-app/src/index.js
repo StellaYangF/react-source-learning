95496b36017e6de67f2e61bdf0cd15a1511a47a2
StellaYangFan <1062878468@qq.com>
2019-12-27 15:53:12 +0800

    Refs usage string, function, React.createRef

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,20 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Button extends React.Component{
-  state = { number: 0, name: 'stella' };
+/** 
+ * Refs: Refs provide a way to access DOM nodes or React elements created in the render method. 
+ * ref三种形式：
+ *  字符串 legacy
+ *  函数 not recommended
+ *  React.createRef() 调用结果 recommended useRef
+*/
+
+/**
+ * as string
+ * */ 
+// class Calculator extends React.Component{
+//   add = () => {
+//     let num1 = parseInt(this.refs.num1.value),
+//         num2 = parseInt(this.refs.num2.value);
+//     this.refs.result.value = num1 + num2;
+//   }
+//   render() {
+//     return (
+//       <>
+//         <input ref='num1' /> + <input ref='num2' /> <button onClick={this.add} >=</button> <input className='input_lg' ref='result' />
+//       </>
+//     )
+//   }
+// }
+
+/** 
+ * as function
+ * 该函数会在virtual DOM挂载插入到页面中后执行，参数为该dom元素实例 
+*/
+// class Calculator extends React.Component{
+//   add = () => {
+//     let num1 = parseInt(this.num1.value) || 0,
+//         num2 = parseInt(this.num2.value) || 0;
+//     this.result.value = num1 + num2;
+//   }
+//   render() {
+//     return (
+//       <>
+//         <input ref={this.num1} /> + <input ref={ this.num2 } /> <button onClick={this.add} >=</button> <input className='input_lg' ref={ this.result } />
+//       </>
+//     )
+//   }
+// }
+
+/** 
+ * as React.createRef() result
+*/
+class Calculator extends React.Component{
+  num1 = React.createRef();
+  num2 = React.createRef();
+  result = React.createRef();
+
   add = () => {
-    this.setState({ number: this.state.number + 1 });
-    this.setState(prevState => ({ number: prevState.number + 1 }), () => console.log(this.state));
-    this.setState(prevState => ({ number: prevState.number + 2 }), () => console.log(this.state));
-    console.log(this.state); // 同步执行
+    let num1 = parseInt(this.num1.current.value) || 0,
+        num2 = parseInt(this.num2.current.value) || 0;
+    this.result.current.value = num1 + num2;
   }
   render() {
-    return <>
-      <h4>{ this.state.number }</h4>
-      <button className='btn btn-info' onClick={this.add}>+</button>
-    </>
+    return (
+      <>
+        <input ref={this.num1} /> + <input ref={ this.num2 } /> <button onClick={this.add} >=</button> <input className='input_lg' ref={ this.result } />
+      </>
+    )
   }
 }
 
-ReactDOM.render(<Button />, document.getElementById('root'));
+class Username extends React.Component{
+  inputRef = React.createRef();
+
+  render () {
+    return <input placeholder='Input your name here' ref={ this.inputRef } style={{ width: 300, marginBottom: 20 }} />
+  }
+}
+
+class Form extends React.Component{
+  username = React.createRef();
+
+  getFocus = () => {
+    this.username.current.inputRef.current.focus()
+  }
+
+  render() {
+    return (
+      <>
+        <Calculator />
+        <br/>
+        <hr/>
+        <Username ref={ this.username }/>
+        <br/>
+        <button onClick={ this.getFocus }>let input get focused</button>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById('root'));
