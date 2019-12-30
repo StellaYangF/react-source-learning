48c15cc36353a7100c742d4f20870670f1b8cced
StellaYangFan <1062878468@qq.com>
2019-12-30 15:21:04 +0800

    createContext vidwo added

--- react-app/docs/11.2.contextPrinciple.js
+++ react-app/docs/11.2.contextPrinciple.js
@@ -37,18 +37,23 @@ const ThemeContext = createContext();
 class Page extends React.Component{
   constructor(props) {
     super(props);
-    this.state = { color: '#d9534f' };
+    this.state = { color: '#d9534f', count: 0 };
   }
 
   changeColor = color => {
     this.setState({ color });
   };
 
+  add = () => this.setState(state => ({ count: state.count + 2 }));
+
+  minus = () => this.setState(state => (state.count ? { count: state.count -1 } : {count: state.count }));
+
   render () {
-    let contextVal = { changeColor: this.changeColor, color: this.state.color };
+    let contextVal = { changeColor: this.changeColor, color: this.state.color, add: this.add, minus: this.minus };
     return (
       <ThemeContext.Provider value={ contextVal }>
         <div style={ { width: 300, padding: 5, margin: '50px auto', border: `4px solid ${this.state.color}` } }>
+          <p> Count: { this.state.count }</p>
           <Header />
           <Main />
         </div>
@@ -111,8 +116,12 @@ class Content extends React.Component{
           value => (
             <div style={{ border: `4px solid ${value.color}`, padding: 10 }}>
               Content
+              <br/>
               <button className='btn btn-danger ml_10' onClick={() => value.changeColor("#d9534f")}>Pink</button>
-            <button className='btn btn-info ml_10' onClick={() => value.changeColor("#46b8da")}>Green</button>
+              <button className='btn btn-info ml_10' onClick={() => value.changeColor("#46b8da")}>Green</button>
+              <br/>
+              <button className='btn btn-secondary ml_10' onClick={ value.minus }>-</button>
+              <button className='btn btn-secondary ml_10' onClick={ value.add }>+</button>
             </div>
           )
         }
