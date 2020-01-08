8be5a5cf9c87ac972170328fbec2c5a406479a11
StellaYangFan <1062878468@qq.com>
2020-01-08 17:32:53 +0800

    saga

--- saga-app/src/store/reducer.js
+++ saga-app/src/store/reducer.js
@@ -5,6 +5,8 @@ export default function(state = initialState, action) {
   switch(action.type) {
     case types.INCREMENT:
       return { number: state.number + 1 };
+    case types.INCREMENT_ASYNC: 
+      return { number: state.number + 2 };
     default:
         return state;
   }
