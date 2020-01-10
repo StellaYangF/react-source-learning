4b1e0804649c39c5d38757448e548b5be2555c86
StellaYangFan <1062878468@qq.com>
2020-01-10 10:54:21 +0800

    react-router applied

--- router-app/src/index.js
+++ router-app/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import User from './components/User';
+import Profile from './components/Profile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <Route path='/' exact component={ Home } />
+    <Route path='/user' component={ User } />
+    <Route path='/profile' component={ Profile } />
+  </Router>
+  ,document.getElementById('root')
+)
