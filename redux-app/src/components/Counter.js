75d8169f46f5b3d7f04cb9bdc9eb207fca221dff
StellaYangFan <1062878468@qq.com>
2020-01-16 09:31:53 +0800

    redux with react usage

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from "react";
 import { createStore } from 'redux';
 export default function() {
   let [ counter, setCounter ] = useState({ number: 0 });
-  let reducer = (state = { number: 0 }, action) => {
+  let reducer = (state = counter, action) => {
     if (action.type === 'increment') return { number: state.number + 1 };
+    else return state;
   }
   let store = createStore(reducer);
 
   useEffect(() => {
-    return store.subscribe(() => setCounter({ number: store.getState().number + 1 }));
+    return store.subscribe(() => setCounter({ number: store.getState().number }));
   })
 
   return (
     <>
-      <p>{ counter.number }</p>
-      <button onClick={ () => setCounter ({ number: counter.number + 1 }) } className='btn btn-primary'>+</button>
+      {/* <p>{ counter.number }</p> */}
+      {/* <button onClick={ () => setCounter ({ number: counter.number + 1 }) } className='btn btn-primary'>+</button> */}
       <br />
-      <p>{ store.getState() }</p>
+      <p>{ counter.number }</p>
       <button onClick={ () => store.dispatch({ type: 'increment' }) } className='btn btn-primary'>+</button>
     </>
   )
