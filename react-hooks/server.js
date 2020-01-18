34d0c2620ae27ea03e1c4987e089cb9966ea700a
StellaYangFan <1062878468@qq.com>
2020-01-18 17:27:30 +0800

    'server'

---
+++ react-hooks/server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    console.log(req);
+    next();
+});
+app.get('/api/users', function (req, res) {
+    let offset = parseInt(req.query.offset);
+    let limit = parseInt(req.query.limit);
+    let result = [];
+    for (let i = offset; i < offset + limit; i++) {
+        result.push({ id: i + 1, name: 'name' + (i + 1) });
+    }
+    res.json(result);
+});
+app.listen(8000);
