75f5bc04e00326c3b89ca878d354ba0366c88777
StellaYangFan <1062878468@qq.com>
2020-01-19 14:24:49 +0800

    antd added

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Counter from './components/Counter1';
 // import Ajax from './components/ajax';
-import Animation from './components/animation';
+// import Animation from './components/animation';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<Animation/>, document.getElementById('root'));
+ReactDOM.render(<DatePicker/>, document.getElementById('root'));
